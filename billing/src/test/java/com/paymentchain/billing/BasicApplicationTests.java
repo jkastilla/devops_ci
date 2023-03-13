667364adb6bc632e1163ac8428c41d9339966d2b
jkastilla <jorge.castilla@atos.net>
2023-03-13 13:51:33 +0100

    add test modificacion 4

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -13,7 +13,7 @@ public class BasicApplicationTests {
  @Test
  public void contextLoads() {
  	    //String meessage= "default message cambio test devops";
-		String meessage= "default message cambio test devops 3";
+		String meessage= "default message cambio test devops 4";
  	    Assert.assertNotNull(meessage);
  }
  
